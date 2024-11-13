6cc6ea1bdd810076113fa1d1470aba215f680394
TDGalea <thomasgalea0@gmail.com>
2024-11-13 01:25:37 +0000

    Improvements all round.
    Better searching method thanks to [Honest_Photograph519 on Reddit](https://www.reddit.com/user/Honest_Photograph519/).
    
    New interactive list if more than one result (after I realised some packages have one base appID, and then expand on that for further packages, therefore preventing us from selecting the main package).

--- flatpak-finder.sh
+++ flatpak-finder.sh
@@ -1,38 +1,119 @@
 #!/bin/bash
+shopt -s nocasematch
 
-# Figure out what we're doing by how we were called. This script should be symlinked/renamed to "fpr" to run, "fpo" to override.
+# Figure out what we're doing by how we were called. This script should be symlinked/renamed to "fpr" to run, "rpo" to override.
 case $0 in
 	*fpr*) mode=run;;
 	*fpo*) mode=override;;
-	    *) printf 'Unrecognised call name! Symlink this script as "fpr" and/or "fpo" for run/override respectively, then run via those symlinks.\n' >&2 && exit 1;;
+	    *) printf 'Unrecognised call name! Symlink this script as "fpr" and/or "fpo" for run/override respectively, then run via those symlinks.\n' >&2 && exit 3;;
 esac
 
-# No app specified.
-[[ -z $1 ]]&& printf "Specify an app to $mode.\n" >&2 && exit 2
+# Nothing passed.
+[[ -z $1 ]] && printf "What Flatpak do you want to $mode?\n" >&2 && exit 2
 
-# Put the search string in $search, put the rest of the arguments in $args.
-search=$1
+# Store the specified app in $pattern, put the rest of the arguments in $args.
+pattern=$1
 shift
 args="$@"
 
-# Generate a list of all Flatpaks found by the given string, and then check how many results we have.
-list="$(flatpak list | sed "s/\t/THIS_IS_A_TAB/g" | grep $search)"
-count="$(printf "$list\n" | wc -l)"
+# Shove all the installed Flatpaks into $paks.
+readarray -t paks < <(flatpak list --columns=name,application)
 
-# Make sure we have only one result.
-[[ $count -gt 1 ]] && printf "Too many results found. Please be a bit more specific:\n" >&2 && printf "$list\n" | sed "s/THIS_IS_A_TAB/\t/g" | column -t >&2 && exit 2
+# Find what Flatpaks match the given pattern.
+for pak in "${paks[@]}"; do
+	IFS=$'\t' read name appid <<<"$pak"
+	[[ $name =~ $pattern ]] || [[ $appid =~ $pattern ]] && {
+		matching_paks+=( "$appid" )
+		printf "Found '$name' ('$appid')\n"
+	}
+done
 
-# Annoyingly, WC is resulting in 1 even if we got ZERO results. So let's just try and fill appID and check that instead.
-appID="$(echo $list | sed "s/THIS_IS_A_TAB/\n/g" | tail -4 | head -1)"
+# Check how many results we have.
+if [[ ${#matching_paks[@]} -gt 1 ]]; then
+	# More than one - go interactive if we can.
+	[[ "$TERM" == "dumb" ]] && {
+		# We're not in a terminal, so we can't go interactive. Try sending a notification, then exit.
+		notify-send "Flatpak Finder" "Too many results (${#matching_paks[@]}).\nPlease be a bit more specific, or run in a terminal to select interactively."
+		exit 1
+	}
+	clear
 
-# Do we actually have an appID?
-[[ "$appID" == "" ]] && printf "No results found. Please check spelling and case.\n" >&2 && exit 2
-printf "Found '$appID'"
+	# Trap CTRL+C so that we can return the terminal to normal.
+	function fq {
+		tput cnorm
+		exit 2
+	}
+	trap 'fq' SIGHUP SIGINT SIGTERM
 
-# Complain if overriding and no instructions provided.
-[[ "$mode" == "override" ]] && [[ -z "$args" ]] && printf " - what do you want to do with it?\n" && exit 2
+	# Check terminal height, and request user to resize if it is too low.
+	function checkTermHeight {
+		while [[ $(tput lines) -lt $(expr ${#matching_paks[@]} + 3) ]]; do
+			printf "Terminal not tall enough!\n"
+			printf "Currently $(tput lines) lines - need $(expr ${#matching_paks[@]} + 3).\n"
+			printf "Resize now, or use CTRL+C to exit.\n"
+			sleep 1
+			clear
+		done
+	}
 
-# Run or override the found flatpak.
-printf ".\n"
-flatpak $mode "$appID" $@
-exit $?
+	# We need an escape code to capture the arrow keys.
+	esc=$(printf "\u1b")
+	# Hide the terminal cursor.
+	tput civis
+
+	selection=1
+	# Loop until an option is chosen.
+	while [[ -z $choice ]]; do
+		clear
+		# Make sure the terminal is tall enough to display all the options.
+		checkTermHeight
+
+		printf "Select a Flatpak from the list below, or use 'q' to quit.\n\n"
+		x=1
+		# Print all the options.
+		for pak in ${matching_paks[@]}; do
+			IFS=$'\t' read appid <<<"$pak"
+			printf " "
+			# If the current option is the highlighted one, invert text colour.
+			[[ $x -eq $selection ]] && printf "\e[30;47m"
+			# Print this option's package name.
+			printf " $appid\e[0m\n"
+			let x+=1
+		done
+
+		unset input
+		# Take user input.
+		read -rsn1 input; [[ $input == $esc ]] && read -rsn2 input
+		case $input in
+			 'q') fq;;
+			'[A') [[ $selection -gt 1 ]] && let selection-=1 || selection=${#matching_paks[@]};; # Up arrow - go up by decreasing the selection var, unless we're at the top, in which case make it the value of matching flatpaks.
+ 			'[B') [[ $selection -lt ${#matching_paks[@]} ]] && let selection+=1 || selection=1;; # Down arrow - go down by increasing the selection var, unless we're at the bottom, in which case make it 1.
+ 			'[C') selection=${#matching_paks[@]};; # Right arrow - select last item.
+			'[D') selection=1;; # Left arrow - Select first item.
+			   *) [[ $input = "" ]] && choice=$selection;; # Enter or space will select an item, otherwise do nothing.
+		esac
+	done
+
+	# Something's been selected - figure out which one it was.
+	x=1
+	for pak in ${matching_paks[@]}; do
+		IFS=$'\t' read appid <<<"$pak"
+		[[ $x -eq $choice ]] && target=$appid
+		let x+=1
+	done
+	tput cnorm
+	printf "\n"
+elif [[ ${#matching_paks[@]} -lt 1 ]]; then
+	# No results at all.
+	printf "No results found. If the Flatpak is installed, please check your spelling.\n"
+	exit 1
+else
+	# Just right.
+	target="$matching_paks"
+fi
+
+# Complain if overriding and no arguments remaining.
+[[ "$mode" == "override" ]] && [[ -z "$args" ]] && printf "What do you want to do with it?\n" && exit 2
+
+# Run or override the found flatpak then preserve the exit code.
+flatpak $mode "$target" $@; exit $?
